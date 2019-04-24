b9fe5d1a0529fc7e0405875e588c06ef4c06a627
LailyS <laily@lailys.com>
2019-04-24 12:54:33 -0700

    Change to basic till the progress is done

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import rootReducer from './rootReducer'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 
